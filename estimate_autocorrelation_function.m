6c5b5e7cd811a0d95b53a8a623f2c9a9c54868bf
Orchisama Das <orchi@cmn15.stanford.edu>
2018-02-27 17:42:20 -0800

    Fixed FFT implementation of autocorrelation function

--- estimate_autocorrelation_function.m
+++ estimate_autocorrelation_function.m
@@ -22,16 +22,24 @@ if strcmp(method,'direct')
     end
     
 elseif strcmp(method,'fft')
-    L = M-1;
-    R_hat = fftshift(ifft(fft(data,L).*fft(fliplr(conj(data)),L)));
-    %filter out bias - remove triangular weighting
-    l = -(L-1)/2:(L-1)/2;
-    R_hat = R_hat./(L-abs(l));
-    %remove noise variance from signal - linear interpolation
-    R_hat((L+1)/2) = (R_hat((L-1)/2) + R_hat((L+1)/2+1))/2;
-    %since ACF is symmetric, preserve positive lags only
-%     R_hat = R_hat((L+1)/2:end);
-%     M = (L+1)/2;
+%     L = M-1;
+%     R_hat = fftshift(ifft(fft(data,L).*fft(fliplr(conj(data)),L)));
+%     %filter out bias - remove triangular weighting
+%     l = -(L-1)/2:(L-1)/2;
+%     R_hat = R_hat./(L-abs(l));
+%     %remove noise variance from signal - linear interpolation
+%     R_hat((L+1)/2) = (R_hat((L-1)/2) + R_hat((L+1)/2+1))/2;
+%     %since ACF is symmetric, preserve positive lags only
+% %     R_hat = R_hat((L+1)/2:end);
+% %     M = (L+1)/2;
+    L = M;
+    nfft = 2^nextpow2(2*M-1);
+    R_hat = ifft( fft(data,nfft) .* conj(fft(data,nfft)) );
+    %# rearrange and keep values corresponding to lags: -(len-1):+(len-1)
+    R_hat = [R_hat(end-M+2:end) , R_hat(1:M)];
+    %remive bias (remove triangular weightings
+    l = [M-1:-1:0, 1:M-1];
+    R_hat = R_hat./(L-l);
 end
 
 % figure;

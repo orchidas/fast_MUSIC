5307d360f4513080d726e5bd863dbd7ffb810715
Orchisama Das <orchi@cmn15.stanford.edu>
2018-04-04 18:44:56 -0700

    New plotting scripts

--- plot_fft.m
+++ plot_fft.m
@@ -7,9 +7,10 @@ files = {'A3.wav    ','A3_1D.wav ','A3_2D.wav ','A3_3D.wav ','A3_23D.wav','A3_13
 fs = 44100;
 nfiles = length(files);
 [b,a] = butter(4,[2500 2900]/(fs/2));
-nsamp = 5*fs;
+nsamp = 6*fs;
 window = blackman(nsamp);
-nbins = 2^nextpow2(nsamp);
+%nbins = 2^nextpow2(nsamp);
+nbins = 2^19;
 tf = zeros(2*nbins,nfiles);
 npeaks = 2;
 all_freqs_found_qifft = zeros(npeaks,nfiles);
@@ -26,14 +27,13 @@ for k = 1:nfiles
     xs = x(fs+(1:nsamp));
     tf(:,k) = fft(ys.*window,2*nbins);% y(fs+(1:nsamp)).*window],2*nbins);
     figure(1); plot(f, 20*log10(abs(tf(1:nbins+1,k))/max(abs(tf(:,k))))); grid; 
-    ylim([-80 0]);
+    ylim([-60 0]);
     %xlim([2660,2680]);
     xlim([2400,2900]);
     %xlim([2.66,2.68]);
     hold on;
 
     %plot spectrogram to see beats
-    figure;
     myspecgram(xs',fs,2047,512,4096);
     ylim([0,5000]);
     
@@ -45,9 +45,11 @@ for k = 1:nfiles
     plot(freqs_found_Hz, peaks, '*'); grid on;hold on;
     
     %do fast Music
-    [peaksfm, freqs_foundfm] = fast_music(ys(1:fmusic_size)',fs,npeaks,100000,'default','fft',files{k});
+    [peaksfm, freqs_foundfm] = fast_music(ys(1:fmusic_size)',fs,npeaks,50000,'default','fft',files{k});
     freqs_foundfm_Hz = freqs_foundfm/pi * (fs/2);
     all_freqs_found_fmusic(k,1:npeaks) = freqs_foundfm_Hz;
+    
+    figure(1);plot(freqs_foundfm_Hz,zeros(2,1),'o');
     
 
 end

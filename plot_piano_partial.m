bca3a5c7bb40052c547c4ab48c812150a724c43e
orchisama <orchisama.das@gmail.com>
2018-06-29 01:13:52 -0700

    New piano plots

--- plot_piano_partial.m
+++ plot_piano_partial.m
@@ -9,7 +9,8 @@ path = '../piano data/A3/';
 file = 'A3.wav';
 fs = 44100;
 [b,a] = butter(4,[2500 2900]/(fs/2));
-nsamps = round(2.^[14:15]);
+ftpower = 14:18;
+nsamps = round(2.^ftpower);
 nns = length(nsamps);
 nbins = 2^19;
 tfs = zeros(2*nbins,nns);
@@ -24,15 +25,14 @@ npeaks = 2;
 y = filtfilt(b,a,x);
 
 for n = 1:nns
-
-nsamp = nsamps(n);
-window = blackman(nsamp);
-ys = y(fs+(1:nsamp));
-tfs(:,n) = fft(ys.*window, 2*nbins);
-[peaksfm, freqs_foundfm] = fast_music(ys',fs,npeaks,500000,'default','fft',file);
-freqs_foundfm = sort(freqs_foundfm);
-freqs_foundfm_L(n) = freqs_foundfm(1)/pi * (fs/2);
-freqs_foundfm_U(n) = freqs_foundfm(2)/pi * (fs/2);
+    nsamp = nsamps(n);
+    ys = y(fs+(1:nsamp));
+    tfs(:,n) = fft(ys, 2*nbins);
+    [peaksfm, freqs_foundfm] = fast_music(ys',fs,npeaks,500000,'default','fft',...
+        strcat(file,'_',num2str(ftpower(n))));
+    freqs_foundfm = sort(freqs_foundfm);
+    freqs_foundfm_L(n) = freqs_foundfm(1);
+    freqs_foundfm_U(n) = freqs_foundfm(2);
 
 end
 
@@ -44,7 +44,7 @@ end
 % xlim([2660,2680]);
 
 %load peak_plots;
-heights = zeros(nns,npeaks);
+% heights = zeros(nns,npeaks);
 for n = 1:nns
     %heights(n,1) = interp1(f',20*log10(abs(tfs(1:nbins+1,n))/max(abs(tfs(:)))), freqs_foundfm_L(n));
     %heights(n,2) = interp1(f',20*log10(abs(tfs(1:nbins+1,n))/max(abs(tfs(:)))), freqs_foundfm_U(n));
@@ -53,7 +53,7 @@ for n = 1:nns
     plot([freqs_foundfm_L(n);freqs_foundfm_L(n)],[-60;0],'b-o');hold on;
     plot([freqs_foundfm_U(n);freqs_foundfm_U(n)],[-60;0],'b-o');hold off;
     title(strcat('Data size = ', num2str(nsamps(n))));
-    xlabel('Frequency in Hz','fontsize',14);
+    xlabel('Frequency in Hz','fontsize',16);
     ylabel('Magnitude in dB','fontsize',16);
     ylim([-60 0]);
     xlim([2660,2680]);
